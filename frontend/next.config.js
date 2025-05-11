a424d7e8fdaab5ddbaa6d976619742f1fadc9c76
nikita-039 <nikita.nik393@gmail.com>
2025-05-11 23:38:16 +0530

    Updated project with latest changes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,11 +4,11 @@ const nextConfig = {
     return [
       {
         source: '/api/segments/:path*',
-        destination: 'https://xeno-crm-4-vlo4.onrender.com/'
+        destination: 'https://xeno-crm-4-vlo4.onrender.com/api/segments/:path*',
       },
       {
-        source: '/api/campaigns/:path*', // ✅ this is what’s missing
-        destination: 'https://xeno-crm-4-vlo4.onrender.com/'
+        source: '/api/campaigns/:path*',
+        destination: 'https://xeno-crm-4-vlo4.onrender.com/api/campaigns/:path*',
       }
     ];
   }
